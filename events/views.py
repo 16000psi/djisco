e2c05861bb13ea4f7addfb934da8757ad5ae9440
16000psi <davidjhnsmith@gmail.com>
2024-03-01 22:46:44 +0000

    Configure basic views

--- events/views.py
+++ events/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
-# Create your views here.
+from .models import Event
+
+
+def event_list(request):
+    return render(request, "events/event_list.html")
+
+
+def event_detail(request, pk):
+    event = get_object_or_404(Event, pk=pk)
+    context = {
+        "event": event,
+    }
+    return render(request, "events/event_detail.html", context)
