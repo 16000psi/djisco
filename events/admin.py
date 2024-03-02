e8090ed588769e63c7cb3bbe14ef57c0439676d9
16000psi <davidjhnsmith@gmail.com>
2024-03-02 11:15:51 +0000

    Add models to admin

--- events/admin.py
+++ events/admin.py
@@ -1,3 +1,9 @@
+# Register your models here.
 from django.contrib import admin
 
-# Register your models here.
+from .models import RSVP, ContributionCommitment, ContributionRequirement, Event
+
+admin.site.register(Event)
+admin.site.register(RSVP)
+admin.site.register(ContributionRequirement)
+admin.site.register(ContributionCommitment)
