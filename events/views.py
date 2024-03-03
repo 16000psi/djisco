aaeec5b08ed95663329b1027e5df1666714d7f6b
16000psi <davidjhnsmith@gmail.com>
2024-03-03 12:51:59 +0000

    Add pagination

--- events/views.py
+++ events/views.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 
+from django.core.paginator import Paginator
 from django.http import Http404
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
@@ -20,8 +21,28 @@ def event_list(request, when="future"):
     else:
         raise Http404("Page not found")
 
+    pagination_limit = 5
+    paginator = Paginator(events, pagination_limit)
+
+    try:
+        page_number = int(request.GET.get("page", 1))
+
+    except ValueError:
+        page_number = 1
+
+    page_obj = paginator.get_page(page_number)
+    start_index = page_obj.start_index()
+    end_index = page_obj.end_index()
+
+    pagination_message = (
+        f"Displaying {start_index} - {end_index} of {len(events)} results."
+    )
+
     context = {
-        "events": events,
+        "pagination_limit": pagination_limit,
+        "page_obj": page_obj,
+        "pagination_message": pagination_message,
+        "result_count": len(events),
         "now": timezone.make_aware(datetime.now()),
         "when": when,
         "title": title,
