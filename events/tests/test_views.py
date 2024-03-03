3addbba5756f458c1ee645b995eaeed0d55a7906
16000psi <davidjhnsmith@gmail.com>
2024-03-03 13:23:49 +0000

    Add basic auth views and tests

--- events/tests/test_views.py
+++ events/tests/test_views.py
@@ -3,8 +3,8 @@ from datetime import datetime
 from django.test import TestCase
 from django.urls import reverse
 from django.utils import timezone
-from events.models import Event
 
+from events.models import Event
 from users.models import User
 
 
@@ -34,3 +34,47 @@ class DetailTestCase(TestCase):
         response = self.client.get(url)
 
         self.assertEqual(response.status_code, 200)
+
+
+class LoginViewTestCase(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.new_user = User.objects.create(username="b")
+        cls.new_user.set_password("b")
+        cls.new_user.save()
+
+    def test_login_success(self):
+        """
+        Tests that a user can login and is redirected to the event list
+        view by default upon login.
+        """
+        login_url = reverse("login")
+        response = self.client.post(
+            login_url, {"username": self.new_user.username, "password": "b"}
+        )
+
+        self.assertRedirects(response, reverse("event_list"))
+        self.assertTrue(response.wsgi_request.user.is_authenticated)
+
+    def test_signed_out_user_navigation(self):
+        """
+        The header navigation should contain options to log in and sign up
+        if a user is not logged in, but not an option to sign out.
+        """
+        response = self.client.get(reverse("event_list"))
+
+        self.assertContains(response, "Log In")
+        self.assertContains(response, "Sign Up")
+        self.assertNotContains(response, "Log Out")
+
+    def test_signed_in_user_navigation(self):
+        """
+        The header navigation should contain the option to sign out if a
+        user is logged in, but not an option to log in or sign up.
+        """
+        self.client.login(username=self.new_user.username, password="b")
+        response = self.client.get(reverse("event_list"))
+
+        self.assertNotContains(response, "Log In")
+        self.assertNotContains(response, "Sign Up")
+        self.assertContains(response, "Log Out")
--- events/urls.py
+++ events/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
+    path("accounts/", include("django.contrib.auth.urls")),
     path("events/", views.event_list, name="event_list"),
     path("events/<int:pk>/", views.event_detail, name="event_detail"),
     path("events/<str:when>/", views.event_list, name="event_list"),
