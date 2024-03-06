406466cc7e4c6c3df5a041dfe661a5413a2a988c
16000psi <davidjhnsmith@gmail.com>
2024-03-06 15:02:26 +0000

    Add ContributionItem to admin

--- events/admin.py
+++ events/admin.py
@@ -1,9 +1,16 @@
 # Register your models here.
 from django.contrib import admin
 
-from .models import RSVP, ContributionCommitment, ContributionRequirement, Event
+from .models import (
+    RSVP,
+    ContributionCommitment,
+    ContributionItem,
+    ContributionRequirement,
+    Event,
+)
 
 admin.site.register(Event)
 admin.site.register(RSVP)
 admin.site.register(ContributionRequirement)
 admin.site.register(ContributionCommitment)
+admin.site.register(ContributionItem)
