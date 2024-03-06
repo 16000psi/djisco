e16ac8c4b126dfb4f60de2b4716c3a8ff9bc98ad
16000psi <davidjhnsmith@gmail.com>
2024-03-06 14:42:40 +0000

    Add events views tests for attendance ajax

--- events/tests/test_views.py
+++ events/tests/test_views.py
@@ -1,3 +1,4 @@
+from http import HTTPStatus
 from datetime import datetime
 
 from django.test import TestCase
@@ -14,7 +15,7 @@ class ListTestCase(TestCase):
         url = reverse("event_list")
         response = self.client.get(url)
 
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
 
 
 class DetailTestCase(TestCase):
@@ -35,7 +36,7 @@ class DetailTestCase(TestCase):
         url = reverse("event_detail", args=[self.event.pk])
         response = self.client.get(url)
 
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
 
 
 class GetAttendeeAggregateTestCase(TestCase):
@@ -183,7 +184,7 @@ class ManageEventAttendanceTestCase(TestCase):
         response = self.client.post(
             reverse("event_attendance", args=[self.event_01.pk, "attend"])
         )
-        self.assertEqual(response.status_code, 403)
+        self.assertEqual(response.status_code, HTTPStatus.FORBIDDEN)
 
     def test_bad_request_raised_if_get_method(self):
         """
@@ -198,7 +199,7 @@ class ManageEventAttendanceTestCase(TestCase):
         response = self.client.get(
             reverse("event_attendance", args=[self.event_01.pk, "attend"])
         )
-        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.status_code, HTTPStatus.BAD_REQUEST)
 
     def test_attending_RSVP_created_if_none_exist_for_authenticated_user(self):
         """
@@ -298,6 +299,206 @@ class ManageEventAttendanceTestCase(TestCase):
             {"redirect_target": "/previous_view/"},
         )
 
-        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.status_code, HTTPStatus.FOUND)
 
         self.assertEqual(response.url, "/previous_view/")
+
+    def test_ajax_attend_200_response_with_success(self):
+        """
+        AJAX manage_event_attendance attend should respond with 200 and correct data
+
+        manage_event_attendance view should return a 200 if the AJAX POST request is
+        successful for the attend path. The response content should also contain a key,
+        "success", with a value of True.
+        """
+        self.client.login(username="b", password="b")
+
+        response = self.client.post(
+            reverse("event_attendance", args=[self.event_01.pk, "attend"]),
+            {"redirect_target": "/previous_view/"},
+            HTTP_ACCEPT="application/json",
+        )
+
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        data = response.json()
+        self.assertIn("success", data)
+        self.assertTrue(data["success"])
+
+    def test_ajax_attend_rsvp_created_if_none_exist(self):
+        """
+        AJAX attend endpoint should create an RSVP record for a valid request
+
+        for a valid request to the attend endpoint, an RSVP record should be created
+        corresponding to the relevant User and Event.
+        """
+        self.client.login(username="b", password="b")
+        initial_rsvp_count = RSVP.objects.count()
+
+        self.client.post(
+            reverse("event_attendance", args=[self.event_01.pk, "attend"]),
+            {"redirect_target": "/previous_view/"},
+            HTTP_ACCEPT="application/json",
+        )
+
+        expected_rsvp_count = initial_rsvp_count + 1
+        final_rsvp_count = RSVP.objects.count()
+        self.assertEqual(expected_rsvp_count, final_rsvp_count)
+
+        last_record = RSVP.objects.latest("id")
+        self.assertEqual(self.event_01.id, last_record.event_id)
+        self.assertEqual(self.user_01.id, last_record.user_id)
+
+    def test_ajax_attend_409_response_with_success_false_if_RSVP_exists(self):
+        """
+        AJAX attend endpoint should respond with 409 and correct data if RSVP exists
+
+        manage_event_attendance view should return a 409 if the AJAX POST request is
+        unsuccessful for the attend path in the case that there is already a matching rsvp
+        record. The response content should also contain a key, "success", with a value of False.
+        The content should also contain an "error_message" key.
+        """
+        self.client.login(username="c", password="c")
+
+        response = self.client.post(
+            reverse("event_attendance", args=[self.event_01.pk, "attend"]),
+            {"redirect_target": "/previous_view/"},
+            HTTP_ACCEPT="application/json",
+        )
+
+        self.assertEqual(response.status_code, HTTPStatus.CONFLICT)
+        data = response.json()
+        self.assertIn("success", data)
+        self.assertFalse(data["success"])
+        self.assertIn("error_message", data)
+
+    def test_ajax_attend_rsvp_not_created_if_already_exists(self):
+        """
+        AJAX unattend endpoint should not create an RSVP record if one exists
+
+        for a valid request to the unattend endpoint, an RSVP record not should be
+        created if one aleady exists for the relevant User and Event.
+        """
+        self.client.login(username="c", password="c")
+        initial_rsvp_count = RSVP.objects.count()
+
+        self.client.post(
+            reverse("event_attendance", args=[self.event_01.pk, "attend"]),
+            {"redirect_target": "/previous_view/"},
+            HTTP_ACCEPT="application/json",
+        )
+
+        expected_rsvp_count = initial_rsvp_count
+        final_rsvp_count = RSVP.objects.count()
+        self.assertEqual(expected_rsvp_count, final_rsvp_count)
+
+    def test_ajax_unattend_200_response_with_success(self):
+        """
+        AJAX unattend endpoint should respond with 200 and correct data
+
+        manage_event_attendance view should return a 200 if the AJAX POST request is
+        successful for the unattend path. The response content should also contain a key,
+        "success", with a value of True.
+        """
+        self.client.login(username="c", password="c")
+
+        response = self.client.post(
+            reverse("event_attendance", args=[self.event_01.pk, "unattend"]),
+            {"redirect_target": "/previous_view/"},
+            HTTP_ACCEPT="application/json",
+        )
+
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        data = response.json()
+        self.assertIn("success", data)
+        self.assertTrue(data["success"])
+
+    def test_ajax_unattend_rsvp_removed_if_exists(self):
+        """
+        AJAX unattend endpoint should remove relevant RSVP record if one exists
+
+        for a valid request to the unattend endpoint, an RSVP record should be
+        removed if it exists for the relevant User and Event.
+        """
+        self.client.login(username="c", password="c")
+        initial_rsvp_count = RSVP.objects.count()
+
+        self.client.post(
+            reverse("event_attendance", args=[self.event_01.pk, "unattend"]),
+            {"redirect_target": "/previous_view/"},
+            HTTP_ACCEPT="application/json",
+        )
+
+        expected_rsvp_count = initial_rsvp_count - 1
+        final_rsvp_count = RSVP.objects.count()
+        self.assertEqual(expected_rsvp_count, final_rsvp_count)
+
+    def test_ajax_unattend_409_response_with_success_false_if_RSVP_does_not_exist(self):
+        """
+        AJAX unattend endpoint should respond with 409 if no rsvp exists
+
+        manage_event_attendance view should return a 409 if the AJAX POST request is
+        unsuccessful for the unattend path in the case that there is not a matching rsvp
+        record. The response content should also contain a key, "success", with a value of False.
+        The content should also contain an "error_message" key.
+        """
+        self.client.login(username="b", password="b")
+
+        response = self.client.post(
+            reverse("event_attendance", args=[self.event_01.pk, "unattend"]),
+            {"redirect_target": "/previous_view/"},
+            HTTP_ACCEPT="application/json",
+        )
+
+        self.assertEqual(response.status_code, HTTPStatus.CONFLICT)
+        data = response.json()
+        self.assertIn("success", data)
+        self.assertFalse(data["success"])
+        self.assertIn("error_message", data)
+
+    def test_ajax_400_response_with_success_false_if_get_request(self):
+        """
+        AJAX manage_event_attendance should respond with 400 if requests is GET
+
+        manage_event_attendance view should return a 400 if the AJAX request is
+        GET. The response content should also contain a key, "success", with a value of
+        False. The content should also contain an "error_message" key.
+        """
+        self.client.login(username="b", password="b")
+
+        actions = ["unattend", "attend"]
+        for action in actions:
+            with self.subTest(action=action):
+                response = self.client.get(
+                    reverse("event_attendance", args=[self.event_01.pk, action]),
+                    {"redirect_target": "/previous_view/"},
+                    HTTP_ACCEPT="application/json",
+                )
+
+                self.assertEqual(response.status_code, HTTPStatus.BAD_REQUEST)
+                data = response.json()
+                self.assertIn("success", data)
+                self.assertFalse(data["success"])
+                self.assertIn("error_message", data)
+
+    def test_ajax_403_response_with_success_false_if_not_authenticated(self):
+        """
+        AJAX manage_event_attendance should respond with 403 if no authenticated user
+
+        manage_event_attendance view should return a 403 if the user is not authenticated.
+        The response content should also contain a key, "success", with a value of
+        False. The content should also contain an "error_message" key.
+        """
+        actions = ["unattend", "attend"]
+        for action in actions:
+            with self.subTest(action=action):
+                response = self.client.post(
+                    reverse("event_attendance", args=[self.event_01.pk, action]),
+                    {"redirect_target": "/previous_view/"},
+                    HTTP_ACCEPT="application/json",
+                )
+
+                self.assertEqual(response.status_code, 403)
+                data = response.json()
+                self.assertIn("success", data)
+                self.assertFalse(data["success"])
+                self.assertIn("error_message", data)
