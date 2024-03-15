a913301a23550cd9743d450ffd1361f1c1bd5206
16000psi <davidjhnsmith@gmail.com>
2024-03-15 23:04:26 +0000

    Add tests for requirements_edit_view

--- events/tests/views/test_contribution_views.py
+++ events/tests/views/test_contribution_views.py
@@ -4,6 +4,7 @@ from django.test import TestCase
 from django.urls import reverse
 from django.utils import timezone
 
+from events.forms import CommitmentForm, ContributionEditForm
 from events.models import (
     RSVP,
     ContributionCommitment,
@@ -126,6 +127,125 @@ class RequirmentCreateViewTestCase(TestCase):
             self.assertEqual(instance.contribution_item, previously_existing_item)
 
 
+class RequirmentEditViewTestCase(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.new_user = User.objects.create_user(username="b", password="b")
+        cls.user_not_attending = User.objects.create_user(username="c", password="c")
+        cls.event = Event.objects.create(
+            title="event01",
+            organiser=cls.new_user,
+            contact=cls.new_user,
+            starts_at=timezone.now() + timezone.timedelta(hours=1),
+            ends_at=timezone.now() + timezone.timedelta(hours=2),
+            location="here",
+            description="brillientay",
+            maximum_attendees=20,
+        )
+        # Create item for requirements to reference
+        cls.contribution_item = ContributionItem.objects.create(title="test_example")
+
+        # Create RSVP for test commitments to reference
+        cls.rsvp = RSVP.objects.create(user=cls.new_user, event=cls.event)
+
+        # Define view url
+        cls.url = reverse(
+            "requirement_edit", args=[cls.event.pk, cls.contribution_item.pk]
+        )
+
+        # Create requirements for the test
+        contribution_requirements = [
+            ContributionRequirement(
+                event=cls.event, contribution_item=cls.contribution_item
+            )
+            for _ in range(5)
+        ]
+        ContributionRequirement.objects.bulk_create(contribution_requirements)
+
+        # Create some commitments for the requirements
+        contribution_commitments = [
+            ContributionCommitment(RSVP=cls.rsvp, contribution_requirement=requirement)
+            for i, requirement in enumerate(contribution_requirements)
+            if i < 3
+        ]
+        ContributionCommitment.objects.bulk_create(contribution_commitments)
+
+    def test_forbidden_for_unauthenticated_user(self):
+        response = self.client.post(self.url, {"quantity": 500})
+
+        self.assertEqual(response.status_code, HTTPStatus.FORBIDDEN)
+        self.assertIn(
+            "Unauthorised to modify event requirements", response.content.decode()
+        )
+
+    def test_forbidden_if_not_event_organiser(self):
+        self.client.force_login(self.user_not_attending)
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, HTTPStatus.FORBIDDEN)
+
+    def test_get_request_happy_path(self):
+        self.client.login(username="b", password="b")
+        response = self.client.get(self.url)
+
+        self.assertEqual(response.status_code, 200)
+
+        self.assertIsInstance(response.context["form"], ContributionEditForm)
+
+        self.assertEqual(response.context["event"], self.event)
+        self.assertEqual(response.context["contribution_item"], self.contribution_item)
+
+        self.assertContains(response, 'name="quantity"')
+
+    def test_post_request_with_fewer_requirements_than_committed_no_db_change(self):
+        self.client.login(username="b", password="b")
+        initial_requirements_count = ContributionRequirement.objects.count()
+
+        # Act - post a quantity lower than already committed
+        response = self.client.post(self.url, {"quantity": 1})
+
+        # Assert - No change to the number of requirements
+        final_requirements_count = ContributionRequirement.objects.count()
+        self.assertEqual = (initial_requirements_count, final_requirements_count)
+
+    def test_post_request_happy_path_reduced_requirements(self):
+        # Arrange
+        new_requirement_quantity = 10
+        self.client.force_login(self.new_user)
+
+        # Act
+        response = self.client.post(self.url, {"quantity": new_requirement_quantity})
+
+        # Assert - Redirects to detail view
+        self.assertEqual(response.status_code, HTTPStatus.FOUND)
+        self.assertRedirects(response, reverse("event_detail", args=[1]))
+
+        # Assert - Creates correct number of new ContributionCommitments
+        final_requirments_count = ContributionRequirement.objects.count()
+        self.assertEqual(
+            new_requirement_quantity,
+            final_requirments_count,
+        )
+
+    def test_post_request_happy_path_increased_requirements(self):
+        # Arrange
+        new_requirement_quantity = 4
+        self.client.force_login(self.new_user)
+
+        # Act
+        response = self.client.post(self.url, {"quantity": new_requirement_quantity})
+
+        # Assert - Redirects to detail view
+        self.assertEqual(response.status_code, HTTPStatus.FOUND)
+        self.assertRedirects(response, reverse("event_detail", args=[1]))
+
+        # Assert - Creates correct number of new ContributionCommitments
+        final_requirments_count = ContributionRequirement.objects.count()
+        self.assertEqual(
+            new_requirement_quantity,
+            final_requirments_count,
+        )
+
+
 class CommitmentCreateViewTestCase(TestCase):
     @classmethod
     def setUpTestData(cls):
@@ -178,6 +298,7 @@ class CommitmentCreateViewTestCase(TestCase):
         # Assert - the context contains the expected forms and objects
         self.assertIn("form", response.context)
         self.assertEqual(response.context["event"], self.event)
+        self.assertIsInstance(response.context["form"], CommitmentForm)
 
     def test_post_request_no_change_if_all_requirements_fulfilled(self):
         # Arrange
